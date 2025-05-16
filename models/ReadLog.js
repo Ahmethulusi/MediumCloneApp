587b6fa8218893107fb9aaa76c872eddb45384fc
Ahmet Hulusi Yumuk <114864848+Ahmethulusi@users.noreply.github.com>
2025-05-16 21:31:39 +0300

    Delete models directory

--- models/ReadLog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const readLogSchema = new mongoose.Schema({
-  articleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Article', required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // optional
-  readAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('ReadLog', readLogSchema);
